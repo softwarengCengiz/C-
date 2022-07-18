65c73de941b5ab07a3f7ffd9382c3fcffdf6e5a8
softwarengCengiz <107462800+softwarengCengiz@users.noreply.github.com>
2022-07-18 23:16:49 +0300

    MiniProject5

---
+++ MiniProject5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace MiniProject5
+{
+    class Program
+    {
+
+        static void Mix(string str){
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                 Console.Write(str[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string? words;
+            Console.Write("Bir İfade Girin :");
+            words = Console.ReadLine();
+
+            Mix(words);
+        }     
+    }
+
+}
