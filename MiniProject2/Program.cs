6a81ea5132542cf5f1b7c5eab0d230b30c3ce082
softwarengCengiz <107462800+softwarengCengiz@users.noreply.github.com>
2022-07-18 22:14:07 +0300

    MiniProject2

---
+++ MiniProject2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MiniProject2
+{
+    class program
+    {    
+
+        static void DrawIt(int x){
+            for(int i = 1; i <= x; i++){
+                
+                for(int j = 0; j <= i; j++){
+                    Console.WriteLine(" ");
+                }
+
+                for (int k = 0; k < 2*i-1; k++)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int boyut;
+            Console.WriteLine("Lütfen Boyut Giriniz:");
+            boyut = Convert.ToInt32(Console.ReadLine());
+
+            DrawIt(boyut);
+        }
+    }
+}
