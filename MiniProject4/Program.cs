8045944ad15c363ddedd1a4c5d1024f03aa9c954
softwarengCengiz <107462800+softwarengCengiz@users.noreply.github.com>
2022-07-18 23:08:58 +0300

    MiniProject4

---
+++ MiniProject4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace MiniProject4
+{
+    class Program
+    {
+
+        static void Fix(string? str, int x){
+            char[] arrStr = str.ToCharArray();
+            arrStr = arrStr.Where((source, index) => index != x).ToArray();         
+            Console.WriteLine(arrStr);
+        }
+
+        static void Main(string[] args)
+        {
+            string? words;
+            int index;
+
+            Console.Write("Bir Kelime Girin :");
+            words = Console.ReadLine();
+
+            Console.Write("Bir Index Girin :");
+            index = Convert.ToInt16(Console.ReadLine());
+
+            Fix(words,index);
+        }     
+    }
+
+}
