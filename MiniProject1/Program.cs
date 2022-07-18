ab6e8677a248a431a1f8782a7bed8ba35f9ceafc
softwarengCengiz <107462800+softwarengCengiz@users.noreply.github.com>
2022-07-18 21:47:24 +0300

    MiniProjectOne

---
+++ MiniProject1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MiniProject1
+{
+    class program
+    {
+
+        static int Fibonacci(int x){
+            int one = 0;
+            int second = 1;
+            int i = 1;
+            int left;
+            int fiboToplam = 0;
+            while(i <= x){
+                left = one + second; //left = 1-2-3-5-8
+                one = second; // one = 1-1-2-3-5
+                second = left; // second = 1-2-3-5-8
+                fiboToplam += one;
+                i++;
+            }
+            Console.WriteLine("Toplam : "+ fiboToplam);
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int derinlik;
+            Console.WriteLine("Lütfen Derinlik Giriniz:");
+            derinlik = Convert.ToInt32(Console.ReadLine());
+            Fibonacci(derinlik);
+        }
+    }
+}
